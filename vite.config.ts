50c7d86f4244474e1b7122c52811ec1e0ad25ed2
Rico K <koivrico@gmail.com>
2025-08-26 11:34:54 +0200

    Fix bundling

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
 	plugins: [
 		dts({
 			insertTypesEntry: true,
+			exclude: ['**/*.test.ts', '**/*.spec.ts'],
 		}),
 		compression({
 			algorithms: ['gzip', 'brotliCompress'],
